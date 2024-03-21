75ebd6bba7c87695c77e54742c05e1de70739989
SarvarbekNematov <meoff1011@gmail.com>
2024-03-21 23:02:37 +0500

    songi dars

---
+++ main.js
@@ -0,0 +1,77 @@
+// let count = 70;
+
+// let arr = []
+// const x = (a) => {
+//     for(let i = 1; i < a;i++){
+//         let b = Math.round(Math.random() * 100)
+//         if(b % 2 == 0){
+//             arr.push(b)
+//         }
+
+//     }
+//     return arr
+// }
+// console.log(x(count));
+
+// let str = "-------+----+-+--+-+";
+// let newStr = ''
+// const x = (a) => {
+//     for(let i = 0; i < a.length;i++){
+//         if(a[i] == '+'){
+//             newStr += 0
+//         }
+//         else{
+//             newStr += a[i]
+//         }
+//     }
+//     return newStr
+// }
+
+// console.log(x(str));
+
+// let arr = [321,312,4,3,124,3];
+// let a = prompt("son kiriting")
+// let newArr = arr.filter((value) => value < a)
+
+// console.log(newArr);
+
+// let a = prompt("son kiriting");
+// let arr = []
+// const x = (a) => {
+//     for(let i = 0; i <= 10;i++){
+//         arr.push(a * i)
+//     }
+//     return arr
+// }
+// console.log(x(a));
+
+// let arr = [123,321,4,43,12,35,5,2,]
+// let b = prompt("son kiriting");
+// let newArr = [];
+// const x = (a) => {
+//     for(let i = 0;i < a.length;i++){
+//         if(b > a[i]){
+//             newArr.push(a[i])
+//         }
+//     }
+//     return newArr
+// }
+// console.log(x(arr));
+
+// let a = prompt("son kiriting");
+// let arr = [];
+// const x = (b) => {
+//     for(let i = 1;i < b;i++){
+//         let yiguvchi = 0
+//         for(let y = 2;y < i;y++){
+//             if(i % y != 0){
+//                 yiguvchi++
+//             }
+//         }
+//         if(i - 2 == yiguvchi){
+//             arr.push(i)
+//         }
+//     }
+//     return arr
+// }
+// console.log(x(a));
